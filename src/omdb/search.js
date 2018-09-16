db12430f19a09e30de1496e42389608606d54ccd
Kim Holland <tripleaxis@gmail.com>
2018-09-16 13:04:15 +1000

    feat: add paging info in search response

--- src/omdb/search.js
+++ src/omdb/search.js
@@ -23,18 +23,29 @@ const getSearchParams = (query, page = 1) => ({
  * Expects request in format: /search/{query}/{page}
  */
 module.exports.handler = async ({ pathParameters = {} }, context, callback) => {
-  const { query, page = 1 } = pathParameters;
+  const { query } = pathParameters;
+  const page = parseInt(pathParameters.page || 1);
+  
   if (!query) {
     callback(null, response.badRequest('Missing query parameter'));
     return;
   }
   
+  const payload = {
+    results: [],
+  };
+  
   try {
     const { data } = await axios(API_ENDPOINT, getSearchParams(query, page));
+    const totalPages = Math.ceil(data.totalResults / 10);
+    payload.results = data.Search;
+    payload.next = page < totalPages ? `/omdb/search/${query}/${page + 1}` : null;
+    payload.prev = page > 1 ? `/omdb/search/${query}/${page - 1}` : null;
+    payload.totalPages = totalPages;
     
-    callback(null, response.ok(data.Search || []));
   } catch (err) {
     console.error(err);
-    callback(null, response.ok([]));
   }
+  
+  callback(null, response.ok(payload));
 };
